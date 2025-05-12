75fb4adcc2101f47d22c4916d37dd1c06c984479
carol <carol.z.hanna@gmail.com>
2025-05-12 22:45:18 +0100

    Reconstruction of bug

--- solr/core/src/java/org/apache/solr/security/RuleBasedAuthorizationPluginBase.java
+++ solr/core/src/java/org/apache/solr/security/RuleBasedAuthorizationPluginBase.java
@@ -120,9 +120,6 @@ public abstract class RuleBasedAuthorizationPluginBase
 
   /** Retrieves permission names for a given set of roles */
   public Set<String> getPermissionNamesForRoles(Set<String> roles) {
-    if (roles == null) {
-      return Set.of();
-    }
     return roles.stream()
         .filter(roleToPermissionsMap::containsKey)
         .flatMap(r -> roleToPermissionsMap.get(r).stream())
