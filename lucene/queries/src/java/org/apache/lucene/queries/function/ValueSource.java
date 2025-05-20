1170ed932c8e0375f63f8c9bd614f2a184118526
carol <carol.z.hanna@gmail.com>
2025-05-20 17:50:58 +0100

    Reconstructing bug

--- lucene/queries/src/java/org/apache/lucene/queries/function/ValueSource.java
+++ lucene/queries/src/java/org/apache/lucene/queries/function/ValueSource.java
@@ -32,6 +32,7 @@ import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.LongValues;
 import org.apache.lucene.search.LongValuesSource;
 import org.apache.lucene.search.Scorable;
+import org.apache.lucene.search.Scorer;
 import org.apache.lucene.search.SimpleFieldComparator;
 import org.apache.lucene.search.SortField;
 
@@ -284,7 +285,7 @@ public abstract class ValueSource {
 
     @Override
     public FunctionValues getValues(Map context, LeafReaderContext readerContext) throws IOException {
-      Scorable scorer = (Scorable) context.get("scorer");
+      Scorer scorer = (Scorer) context.get("scorer");
       DoubleValues scores = scorer == null ? null : DoubleValuesSource.fromScorer(scorer);
 
       IndexSearcher searcher = (IndexSearcher) context.get("searcher");
